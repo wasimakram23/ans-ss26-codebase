75cbece94b7afa185594f9c63be5aa2b7426bde9
wasim <wasimruet12@gmail.com>
2026-04-24 17:03:50 +0200

    Lab 0 mininet configuration done with a simple bridge network

--- lab0/network_topo.py
+++ lab0/network_topo.py
@@ -1,35 +1,37 @@
-"""
- Copyright (c) 2026 Computer Networks Group @ UPB
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of
- this software and associated documentation files (the "Software"), to deal in
- the Software without restriction, including without limitation the rights to
- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- the Software, and to permit persons to whom the Software is furnished to do so,
- subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- """
-
-#!/usr/bin/python
-
+from mininet.net import Mininet
 from mininet.topo import Topo
+from mininet.link import TCLink
+from mininet.node import OVSBridge
+from mininet.cli import CLI
 
 class BridgeTopo(Topo):
-    "Creat a bridge-like customized network topology according to Figure 1 in the lab0 description."
-
     def __init__(self):
-
         Topo.__init__(self)
 
-        # TODO: add nodes and links to construct the topology; remember to specify the link properties
+        # Hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
 
-topos = {'bridge': (lambda: BridgeTopo())}
+        # Switches as pure bridges
+        s1 = self.addSwitch('s1', cls=OVSBridge)
+        s2 = self.addSwitch('s2', cls=OVSBridge)
+
+        # Host–switch links
+        self.addLink(h1, s1, bw=15, delay='10ms', cls=TCLink)
+        self.addLink(h2, s1, bw=15, delay='10ms', cls=TCLink)
+        self.addLink(h3, s2, bw=15, delay='10ms', cls=TCLink)
+        self.addLink(h4, s2, bw=15, delay='10ms', cls=TCLink)
+
+        # Inter-switch link
+        self.addLink(s1, s2, bw=20, delay='45ms', cls=TCLink)
+
+topo = BridgeTopo()
+
+# IMPORTANT: controller=None disables OpenFlow and prevents c0 from appearing
+net = Mininet(topo=topo, controller=None)
+
+net.start()
+CLI(net)
+net.stop()
